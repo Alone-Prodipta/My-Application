b7614f64c4aba9c24b9c66c68e34761b09fd1844
Prodipta <nayebprodipta@gmail.com>
2026-05-06 23:25:28 +0530

    customized the login and sign in page

--- travel_app/lib/profile.dart
+++ travel_app/lib/profile.dart
@@ -1,69 +1,47 @@
 import 'package:flutter/material.dart';
+import 'profile_login.dart';
+import 'profile_signin.dart';
 
-class CountriesPage extends StatelessWidget {
-  const CountriesPage({super.key});
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 40),
+    return DefaultTabController(
+      length: 2, // Number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          actions: const [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: CircleAvatar(
-                radius: 100,
-                child: Icon(  Icons.person,size: 100),
+                radius: 50,
+                child: Icon(
+                  Icons.person,
+                  size: 50,
+                ),
               ),
             ),
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Sign in'),
+              Tab(text: 'Log in'),
+            ],
           ),
-          SizedBox(height: 0),
-          SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(50),),
-                    ),
-                    labelText: 'Email',
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                    ),
-                    labelText: 'Password',
-                  ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  alignment: Alignment.center,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print('Login button pressed');
-                    },
-
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                      ),
-                      child:Text(
-                        'Login',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
+        body:const TabBarView(
+          children: [
+            SigninPage(),
+            LoginPage(),
+          ],
+        ),
       ),
     );
   }
